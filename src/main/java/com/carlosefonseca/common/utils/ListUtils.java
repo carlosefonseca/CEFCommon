0feef5debd1419d9fb1ac7cf5266d31cf62fda08
Carlos Fonseca <carlosefonseca@gmail.com>
2014-05-18 12:32:41 +0100

    ListComparator changes

--- src/main/java/com/carlosefonseca/common/utils/ListUtils.java
+++ src/main/java/com/carlosefonseca/common/utils/ListUtils.java
@@ -3,10 +3,7 @@ package com.carlosefonseca.common.utils;
 import android.os.Build;
 import android.util.SparseArray;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 import static com.carlosefonseca.common.utils.CodeUtils.LONG_L;
 import static com.carlosefonseca.common.utils.CodeUtils.SIDE_T;
@@ -88,30 +85,72 @@ public final class ListUtils {
     }
 
     public static class ListComparator<T> {
+        private static final java.lang.String TAG = CodeUtils.getTag(ListComparator.class);
+
         public final List<T> newObjects;  // to create
         public final List<T> sameObjects; // to do nothing
+        public final List<T> updatedObjects; // to do nothing
         public final List<T> oldObjects;  // to delete
 
-        ListComparator(List<T> newObjects, List<T> sameObjects, List<T> oldObjects) {
+        ListComparator(List<T> newObjects, List<T> sameObjects, List<T> oldObjects, List<T> updatedObjects) {
             this.newObjects = newObjects;
             this.sameObjects = sameObjects;
+            this.updatedObjects = updatedObjects;
             this.oldObjects = oldObjects;
         }
 
+        /**
+         * Processes two lists and separates the items only on the first, the items only on the second and items on both. If an
+         * item is on both, uses {@link java.lang.Comparable#compareTo(Object)} to check if the item on the second is greater than
+         * the one on the first list.
+         *
+         * @param <T> Should implement {@link java.lang.Object#equals(Object)}
+         */
+        public static <T extends Comparable<T>> ListComparator<T> computeWithUpdates(Collection<T> existingStuff,
+                                                                                     Collection<T> newStuff) {
+            ArrayList<T> oldObjects = new ArrayList<>(existingStuff);
+            ArrayList<T> newObjects = new ArrayList<>();
+            ArrayList<T> updatedObjects = new ArrayList<>();
+            ArrayList<T> sameObjects = new ArrayList<>();
+
+            for (T t : newStuff) {
+                final int i = oldObjects.indexOf(t);
+                if (i < 0) {
+                    newObjects.add(t);
+                } else {
+                    final T oldT = oldObjects.remove(i);
+                    final int compareTo = oldT.compareTo(t);
+                    if (compareTo == 0) {
+                        sameObjects.add(t);
+                    } else if (compareTo > 0) {
+                        updatedObjects.add(t);
+                    } else {
+                        Log.w(TAG, "Existing object " + oldT + " is newer than 'new' object " + t);
+                    }
+                }
+            }
+            return new ListComparator<>(newObjects, sameObjects, oldObjects, updatedObjects);
+        }
+
+        /**
+         * Processes two lists and separates the items only on the first, the items only on the second and items on both.
+         *
+         * @param <T> Should implement {@link java.lang.Object#equals(Object)}
+         * @see {@link #computeWithUpdates(java.util.Collection, java.util.Collection)}
+         */
         public static <T> ListComparator<T> compute(Collection<T> existingStuff, Collection<T> newStuff) {
             ArrayList<T> oldObjects = new ArrayList<>(existingStuff);
             ArrayList<T> newObjects = new ArrayList<>();
             ArrayList<T> sameObjects = new ArrayList<>();
 
             for (T t : newStuff) {
-                final boolean removed = oldObjects.remove(t);
-                if (removed) {
+                if (oldObjects.remove(t)) {
                     sameObjects.add(t);
                 } else {
                     newObjects.add(t);
                 }
             }
-            return new ListComparator<>(newObjects, sameObjects, oldObjects);
+            return new ListComparator<>(newObjects, sameObjects, oldObjects, null);
         }
 
         /**
@@ -148,5 +187,15 @@ public final class ListUtils {
                                : !oldObjects.isEmpty() ? oldObjects.get(0) : !sameObjects.isEmpty() ? sameObjects.get(0) : null;
             return instance != null ? instance.getClass().getSimpleName() : "<Empty List?>";
         }
+    }
+
+    public interface Id {
+        int getId();
+    }
+
+    public static ArrayList<Integer> getIds(Iterable<? extends Id> list) {
+        final ArrayList<Integer> integers = new ArrayList<>();
+        for (Id item : list) integers.add(item.getId());
+        return integers;
     }
 }
