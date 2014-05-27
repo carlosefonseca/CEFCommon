d029ae92f1f56c104d5b2acfc3f9faad64733802
Carlos Fonseca <carlosefonseca@gmail.com>
2014-05-28 00:48:49 +0100

    TaskUtils stuff

--- src/main/java/com/carlosefonseca/common/utils/TaskUtils.java
+++ src/main/java/com/carlosefonseca/common/utils/TaskUtils.java
@@ -2,10 +2,29 @@ package com.carlosefonseca.common.utils;
 
 import android.os.Handler;
 import android.os.Looper;
+import bolts.AggregateException;
 import bolts.Continuation;
 import bolts.Task;
 
 public final class TaskUtils {
+    private static final String TAG = CodeUtils.getTag(TaskUtils.class);
+
+    public static final Continuation<Void, Void> LogErrorContinuation = new Continuation<Void, Void>() {
+        @Override
+        public Void then(Task<Void> voidTask) throws Exception {
+            final Exception error = voidTask.getError();
+            if (error != null) {
+                Log.w(TAG, error);
+                if (error instanceof AggregateException) {
+                    for (Exception exception : ((AggregateException) error).getErrors()) {
+                        Log.w(TAG, exception);
+                    }
+                }
+            }
+            return null;
+        }
+    };
+
     private TaskUtils() {}
 
     public static <T> Task<T> runTaskWithTimeout(Task<T> t, int timeout) {
@@ -28,5 +47,25 @@ public final class TaskUtils {
         });
 
         return taskCompletionSource.getTask();
+    }
+
+    public static <T> Continuation<T, T> getPassThruLogErrorContinuation() {
+        return new Continuation<T, T>() {
+            @Override
+            public T then(Task<T> voidTask) throws Exception {
+                final Exception error = voidTask.getError();
+                if (error != null) {
+                    Log.w(TAG, "Errors during task execution", error);
+                    if (error instanceof AggregateException) {
+                        for (Exception exception : ((AggregateException) error).getErrors()) {
+                            Log.w(TAG, exception);
+                        }
+                    }
+                    throw error;
+                } else {
+                    return voidTask.getResult();
+                }
+            }
+        };
     }
 }
