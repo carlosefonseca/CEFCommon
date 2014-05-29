a9650566fb2af34ad6e123f568259da2edf58cf9
Carlos Fonseca <carlosefonseca@gmail.com>
2014-05-29 18:10:58 +0100

    + RepeatWhileOnActivity

---
+++ src/main/java/com/carlosefonseca/common/utils/RepeatWhileOnActivity.java
@@ -0,0 +1,55 @@
+package com.carlosefonseca.common.utils;
+
+import android.os.Handler;
+import android.os.Looper;
+
+public class RepeatWhileOnActivity implements ActivityStateListener.Interface {
+
+    protected Runnable mRunnable;
+    protected long mTime;
+
+    protected Handler mHandler = new Handler(Looper.getMainLooper());
+    protected boolean mRunning;
+
+    public RepeatWhileOnActivity(Runnable runnable, long time) {
+        this.mRunnable = runnable;
+        this.mTime = time;
+    }
+
+    @Override
+    public void onStart() {
+        Log.v();
+        if (!mRunning && mRunnable != null) {
+            mRunning = true;
+            schedule();
+        }
+    }
+
+    @Override
+    public void onStop(boolean isFinishing) {
+        Log.v();
+        if (mRunning) {
+            mRunning = false;
+            unschedule();
+        }
+    }
+
+    private void unschedule() {
+        mHandler.removeCallbacksAndMessages(null);
+    }
+
+    private void schedule() {
+        mHandler.postDelayed(mInnerRunnable, mTime);
+    }
+
+    protected Runnable mInnerRunnable = new Runnable() {
+        @Override
+        public void run() {
+            if (mRunning) {
+                mRunnable.run();
+                schedule();
+            }
+        }
+    };
+
+}
