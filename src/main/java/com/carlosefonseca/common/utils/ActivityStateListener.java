c15cd672617f6e223a947c201875f41df5b66e15
Carlos Fonseca <carlosefonseca@gmail.com>
2014-05-29 18:10:49 +0100

    + ActivityStateListener

---
+++ src/main/java/com/carlosefonseca/common/utils/ActivityStateListener.java
@@ -0,0 +1,56 @@
+package com.carlosefonseca.common.utils;
+
+import android.app.Activity;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ActivityStateListener {
+
+    protected Set<Interface> mListeners = new HashSet<>();
+
+    protected Activity mActivity;
+    private boolean mRunning;
+
+    public ActivityStateListener(Activity activity) {
+        this.mActivity = activity;
+    }
+
+    public void addListener(Interface listener) {
+        mListeners.add(listener);
+    }
+
+    public void addListenerAndNotify(Interface listener) {
+        mListeners.add(listener);
+        if (mRunning) listener.onStart();
+    }
+
+    public void removeListener(Interface listener) {
+        mListeners.remove(listener);
+    }
+
+    public void onStart() {
+        if (!mRunning) {
+            mRunning = true;
+            for (Interface mListener : mListeners) {
+                mListener.onStart();
+            }
+        }
+    }
+
+    public void onStop() {
+        if (mRunning) {
+            mRunning = false;
+            final boolean finishing = mActivity.isFinishing();
+            for (Interface mListener : mListeners) {
+                mListener.onStop(finishing);
+            }
+        }
+    }
+
+    public interface Interface {
+        public void onStart();
+
+        public void onStop(boolean isFinishing);
+    }
+}
