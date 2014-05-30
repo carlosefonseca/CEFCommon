fc2c2abd66d2a23fc8bfab23a7e5745add65c167
Carlos Fonseca <carlosefonseca@gmail.com>
2014-05-30 13:15:41 +0100

    FoldedBoxShape can also draw a version rotated by 180 (folded piece on the top right corner.

--- src/main/java/com/carlosefonseca/common/shapes/FoldedBoxShape.java
+++ src/main/java/com/carlosefonseca/common/shapes/FoldedBoxShape.java
@@ -12,31 +12,77 @@ import static com.carlosefonseca.common.utils.ImageUtils.dp2px;
  * Creates a balloon shape like those on certain messaging apps.
  */
 public class FoldedBoxShape extends Shape {
+    private final short mOption;
     private Path path;
 
-    public FoldedBoxShape() { }
+    public static final short FOLD_DOWN = 0;
+    public static final short FOLD_UP = 1;
+
+    public FoldedBoxShape() { this(FOLD_DOWN); }
+
+    public FoldedBoxShape(short option) { mOption = option;}
+
+    //  All percentages are relative to the total height of the canvas.
+
+    //  ####################      |
+    //  ####################      |
+    //  ####################      |
+    //  ####################      |
+    //   ##   |1dp                |height (100%)
+    //    ##########  |           |
+    //      ########  |           |
+    //        ######  |h1 (25%)   |
+    //          ####  |           |
+    //            ##  |           |
+    //  |--|10%
+    //  |----------|30%
 
     @Override
     protected void onResize(float width, float height) {
-        int h1 = (int) (height * 0.25);
-        int point = dp2px(1);
-        int hb = (int) (height - h1 - point);
+        if (mOption == FOLD_DOWN) {
+            int point = dp2px(1);
+            int h1 = (int) (height * 0.25);
+            int hb = (int) (height - h1 - point);
 
-        path = new Path();
-        path.moveTo(0, 0);
+            path = new Path();
+            path.moveTo(0, 0);
 
-        path.lineTo(width, 0);                  // right side
-        path.lineTo(width, hb);                 // right side down
-        path.lineTo(height * 0.1f, hb);         // middle top
-        path.lineTo(height * 0.1f, hb + point); // middle bottom
-        path.lineTo(height * .3f, hb + point);  // lower right top
-        path.lineTo(height * .3f, height);      // lower right bottom
-        path.lineTo(0, hb);                     // lower left
-        path.close();
+            path.lineTo(width, 0);                  // right side
+            path.lineTo(width, hb);                 // right side down
+            path.lineTo(height * 0.1f, hb);         // middle top
+            path.lineTo(height * 0.1f, hb + point); // middle bottom
+            path.lineTo(height * .3f, hb + point);  // lower right top
+            path.lineTo(height * .3f, height);      // lower right bottom
+            path.lineTo(0, hb);                     // lower left
+            path.close();
+        } else {
+            int point = dp2px(1);
+            int h1 = (int) (height * 0.25);
+            int hb = (int) (height - h1 - point);
+
+            float iwidth = mOption == FOLD_UP ? width : 0;
+            float iheight = mOption == FOLD_UP ? height : 0;
+
+            path = new Path();
+            path.moveTo(iwidth - 0, iheight - 0);
+
+            path.lineTo(iwidth - width, iheight - 0);                  // right side
+            path.lineTo(iwidth - width, iheight - hb);                 // right side down
+            path.lineTo(iwidth - height * 0.1f, iheight - hb);         // middle top
+            path.lineTo(iwidth - height * 0.1f, iheight - (hb + point)); // middle bottom
+            path.lineTo(iwidth - height * .3f, iheight - (hb + point));  // lower right top
+            path.lineTo(iwidth - height * .3f, iheight - height);      // lower right bottom
+            path.lineTo(iwidth - 0, iheight - hb);                     // lower left
+            path.close();
+        }
     }
 
     @Override
     public void draw(Canvas canvas, Paint paint) {
         canvas.drawPath(path, paint);
+    }
+
+    public static int getPaddingForHeight(float height) {
+        return (int) (height * 0.25) + dp2px(1);
     }
 }
