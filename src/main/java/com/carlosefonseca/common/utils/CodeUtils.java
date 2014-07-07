c59986a63e80c264d18e909e8081dd6bb7cebf34
Carlos Fonseca <carlosefonseca@gmail.com>
2014-07-07 13:51:34 +0100

    CodeUtils.equals - Null-safe equivalent of a.equals(b).

--- src/main/java/com/carlosefonseca/common/utils/CodeUtils.java
+++ src/main/java/com/carlosefonseca/common/utils/CodeUtils.java
@@ -365,4 +365,11 @@ public final class CodeUtils {
             }
         });
     }
+
+    /**
+     * Null-safe equivalent of {@code a.equals(b)}.
+     */
+    public static boolean equals(Object a, Object b) {
+        return (a == null) ? (b == null) : a.equals(b);
+    }
 }
