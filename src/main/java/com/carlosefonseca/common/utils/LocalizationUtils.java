8bc7cb8d13c9e2aee44cca8b103dc1b24945e8b9
Carlos Fonseca <carlosefonseca@gmail.com>
2014-12-18 13:19:22 +0000

    + LocalizationUtils.Lang

--- src/main/java/com/carlosefonseca/common/utils/LocalizationUtils.java
+++ src/main/java/com/carlosefonseca/common/utils/LocalizationUtils.java
@@ -3,7 +3,10 @@ package com.carlosefonseca.common.utils;
 import android.content.res.Configuration;
 import android.content.res.Resources;
 import com.carlosefonseca.common.CFApp;
+import org.jetbrains.annotations.NotNull;
 
+import java.util.HashMap;
+import java.util.List;
 import java.util.Locale;
 
 import static com.carlosefonseca.common.utils.CodeUtils.getTag;
@@ -43,11 +46,68 @@ public final class LocalizationUtils {
         return l.getDisplayName(l);
     }
 
-    protected static Locale getLocale(String languageCode) {
-        if (languageCode.length() == 5 && languageCode.charAt(2) == '-') {
+    public static Locale getLocale(String languageCode) {
+        if (languageCode.length() == 5 && (languageCode.charAt(2) == '-' || languageCode.charAt(2) == '_')) {
             return new Locale(languageCode.substring(0, 2), languageCode.substring(3));
         } else {
             return new Locale(languageCode);
+        }
+    }
+
+    public static class Lang implements Comparable<Lang> {
+
+        private Locale locale;
+        private boolean showRegion;
+        private String stringCache;
+        private String languageCode;
+        private String code;
+
+        public Lang(String code) {
+            this.code = code;
+            locale = getLocale(code);
+            languageCode = locale.getLanguage();
+        }
+
+        public String getCode() {
+            return code;
+        }
+
+        @Override
+        public String toString() {
+            if (stringCache == null) {
+                stringCache = showRegion ? locale.getDisplayName(locale) : locale.getDisplayLanguage(locale);
+            }
+            return stringCache;
+        }
+
+
+        public boolean showRegion() {
+            return showRegion;
+        }
+
+        public void setShowRegion(boolean showRegion) {
+            if (this.showRegion != showRegion) {
+                stringCache = null;
+                this.showRegion = showRegion;
+            }
+        }
+
+        @Override
+        public int compareTo(@NotNull Lang another) {
+            return toString().compareTo(another.toString());
+        }
+
+        public static void setShowRegions(List<Lang> langList) {
+            HashMap<String, Lang> map = new HashMap<>();
+            for (Lang lang : langList) {
+                Lang seen = map.get(lang.languageCode);
+                if (seen == null) {
+                    map.put(lang.languageCode, lang);
+                } else {
+                    seen.setShowRegion(true);
+                    lang.setShowRegion(true);
+                }
+            }
         }
     }
 }
