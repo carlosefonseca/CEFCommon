6febf2664434bbf0a8b16697de174374f022d383
Carlos Fonseca <carlosefonseca@gmail.com>
2015-01-28 23:31:58 +0000

    Description on CFMapView

--- src/main/java/com/carlosefonseca/common/widgets/CFMapView.java
+++ src/main/java/com/carlosefonseca/common/widgets/CFMapView.java
@@ -1,10 +1,18 @@
 package com.carlosefonseca.common.widgets;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.view.MotionEvent;
 import com.google.android.gms.maps.GoogleMapOptions;
 import com.google.android.gms.maps.MapView;
 
+/**
+ * Subclass of MapView that allows for a touch listener to be attached.
+ * This is useful if you need to detect when touches occurred on the map and act on them,
+ * e.g., stop moving the camera that's following the user to allow free motion.
+ *
+ */
+@SuppressLint("ViewConstructor")
 public class CFMapView extends MapView {
     public CFMapView(Context context, GoogleMapOptions googleMapOptions) {super(context, googleMapOptions);}
 
