97e23c26f9203f7a33b46bc3509639b14faaf76b
Carlos Fonseca <carlosefonseca@gmail.com>
2015-04-01 18:25:17 +0100

    CFCalendar improvements

--- src/main/java/com/carlosefonseca/common/utils/CFCalendar.java
+++ src/main/java/com/carlosefonseca/common/utils/CFCalendar.java
@@ -6,6 +6,10 @@ import java.util.GregorianCalendar;
 
 public class CFCalendar extends GregorianCalendar {
 
+    public CFCalendar() {
+        super();
+    }
+
     public CFCalendar(Date date) {
         super();
         setTime(date);
@@ -23,6 +27,37 @@ public class CFCalendar extends GregorianCalendar {
         return get(Calendar.DAY_OF_MONTH);
     }
 
+
+    public CFCalendar addMillis(int millis) {
+        add(Calendar.MILLISECOND, millis);
+        return this;
+    }
+    public CFCalendar addSeconds(int secs) {
+        add(Calendar.SECOND, secs);
+        return this;
+    }
+    public CFCalendar addMinutes(int mins) {
+        add(Calendar.MINUTE, mins);
+        return this;
+    }
+    public CFCalendar addHours(int hours) {
+        add(Calendar.HOUR, hours);
+        return this;
+    }
+    public CFCalendar addDays(int days) {
+        add(Calendar.DAY_OF_MONTH, days);
+        return this;
+    }
+    public CFCalendar addWeeks(int weeks) {
+        add(Calendar.WEEK_OF_YEAR, weeks);
+        return this;
+    }
+    public CFCalendar addMonths(int months) {
+        add(Calendar.MONTH, months);
+        return this;
+    }
+    public CFCalendar addYears(int years) {
+        add(Calendar.YEAR, years);
+        return this;
+    }
 }
-
-
